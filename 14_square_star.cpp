e7be25fd2b2a8d346796d258ae0f99a4e5f2d459
Akash Jaiswal <akashjaiswal.com>
2024-05-12 23:54:09 +0530

    Update

--- 14_square_star.cpp
+++ 14_square_star.cpp
@@ -6,14 +6,14 @@ int main(){
     cout << "Enter the number for Square print of '*' : ";
     cin >> n;
     int i=1;
-    while(i <= n){
-        int j = 1;
-        while (j <= n){
+    while(i <= n){ // we have to print * till n num so it it = in the i<=n suppose this will print the rows 
+        int j = 1; // here we go with the column 
+        while (j <= n){ // we are making j for column which will also be the same 
             cout << "* ";
-            j = j + 1;
+            j = j + 1; // after every star print the while loop will check for the condition and here the value is added by 1 increment;
         }
         cout << endl;
-        i = i + 1;
+        i = i + 1; // Here we have not used the ++ operator as it will go into the infinite loop
     }
     return 0;
 }
