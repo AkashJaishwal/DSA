ef92e75ceed896cc429a005dd088662aca751633
Akash Jaiswal <akashjaiswal.com>
2024-05-13 04:42:32 +0530

    Updated

--- 21_reverse_num_triangle_pattern.cpp
+++ 21_reverse_num_triangle_pattern.cpp
@@ -11,7 +11,7 @@ int main(){
     while(i<=n){
         int j = 1;
         while(j<=i){
-            cout << i- j+1 << " ";
+            cout << i- j+1 << " "; // i-row j-column
             j = j+1;
         }
         cout << endl;
@@ -26,4 +26,5 @@ Eneter a Number: 4
 2 1
 3 2 1
 4 3 2 1
+
 */
