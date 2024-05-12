5c859bc2f8a0275f7304d03ce44d70afa2e924e0
Akash Jaiswal <akashjaiswal.com>
2024-05-13 00:02:10 +0530

    Update

---
+++ 15_num_square_pattern.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Enter the No. for pattern : ";
+    int n;
+    cin >> n;
+
+    int i = 1;
+    while(i<=n)
+    {
+        int j = 1;
+        while(j<=n)
+        {
+            cout << j;
+            j = j+1;
+        }
+        cout << "\n";
+        i = i+1;
+    }
+    return 0;
+}
