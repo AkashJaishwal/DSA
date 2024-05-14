d28327cb3d5d6a7f79c0b744fb5a0a4405563a17
Akash Jaiswal <akashjaiswal334@gmail.com>
2024-05-15 00:40:38 +0530

    Prctice the problem

--- 24_char_count_square_pattern.cpp
+++ 24_char_count_square_pattern.cpp
@@ -1,19 +1,40 @@
 #include <iostream>
 using namespace std;
 
+// int main()
+// {
+//     int n;
+//     cout<< "Enter a num : ";
+//     cin >> n;
+
+//     int i = 1;
+//     char ch= 'A';
+
+//     while(i<=n){
+//         int j = 1;
+//         while(j<=n){
+//             cout <<ch << " ";
+//             ch = ch + 1;
+//             j = j + 1;
+//         }
+//         cout << endl;
+//         i = i + 1;
+//     }
+// }
+
 int main()
 {
     int n;
-    cout<< "Enter a num : ";
+    cout << "Enter a Num : ";
     cin >> n;
 
     int i = 1;
-    char ch= 'A';
+    char ch = 'A';
 
-    while(i<=n){
+    while (i <= n){
         int j = 1;
-        while(j<=n){
-            cout <<ch << " ";
+        while (j <= n){
+            cout << ch << " ";
             ch = ch + 1;
             j = j + 1;
         }
