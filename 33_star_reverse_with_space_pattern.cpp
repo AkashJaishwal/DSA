b7d7860eb5d42f9b2443b999eccc598f4ffe1d42
Akash Jaiswal <akashjaiswal334@gmail.com>
2024-05-20 16:41:13 +0530

    solved q33

--- 33_star_reverse_with_space_pattern.cpp
+++ 33_star_reverse_with_space_pattern.cpp
@@ -9,17 +9,17 @@ int main()
 
     int i = 1;
 
-    while (i <= n)
-    {
-        int space = i - 1;
-        while (space)
+    while (i <= n){
+
+        int space = 1;
+        while(space < i)
         {
             cout << " " << " ";
-            space = space + 2;
+            space = space + 1;
         }
 
         int j = 1;
-        while (j <= i)
+        while (j <= n - i + 1)
         {
             cout << "*" << " ";
             j = j + 1;
@@ -28,3 +28,14 @@ int main()
         i = i + 1;
     }
 }
+
+/*
+Input and Output 
+
+Enter a number : 4
+* * * *
+  * * *
+    * *
+      *
+    
+*/
--- tempCodeRunnerFile.cpp
+++
@@ -1,6 +0,0 @@
-        // int space = i - 1;
-        // while (space)
-        // {
-        //     cout << " " << " ";
-        //     space = space - 1;
-        // }
