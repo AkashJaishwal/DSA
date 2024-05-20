899430f3b803727d080fd28cc3ea5fee27e6106e
Akash Jaiswal <akashjaiswal334@gmail.com>
2024-05-20 20:07:01 +0530

    Solved another problem of Q34

--- 34_num_pattern_as_33.cpp
+++ 34_num_pattern_as_33.cpp
@@ -109,5 +109,41 @@ int main()
     }
 }
 
+*/
+
+/* 
+For pattern 
+Enter a Num : 4
+      1
+    2 3
+  4 5 6
+7 8 9 10
+
+int main()
+{
+    int n;
+    cout << "Enter a Num : ";
+    cin >> n;
+
+    int i = 1;
+    int count = 1;
+    while (i <= n){
+        int j = 1; 
+        int space = n - i;
+
+        while (space){
+            cout << " " << " ";
+            space = space - 1;
+        }
+
+        while (j <= i){
+            cout << count << " ";
+            count = count + 1;
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+}
 
 */
