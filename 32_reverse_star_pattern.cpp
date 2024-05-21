afb9dbec3a323e8bef896216ba40d7c25dbbf9ca
Akash Jaiswal <akashjaiswal334@gmail.com>
2024-05-21 10:19:58 +0530

    Practice session

--- 32_reverse_star_pattern.cpp
+++ 32_reverse_star_pattern.cpp
@@ -30,3 +30,23 @@ Enter a number : 4
 
 
 */
+
+/* 
+Practice session
+int main ()
+{
+    int n;
+    cin >> n;
+
+    int i = 1;
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= n - i + 1){
+            cout << "* ";
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+} */
