28ced0e9437eb94e558088258da219e1879f46f0
Akash Jaiswal <akashjaiswal334@gmail.com>
2024-05-21 10:34:18 +0530

    practice session

--- 33_star_reverse_with_space_pattern.cpp
+++ 33_star_reverse_with_space_pattern.cpp
@@ -39,3 +39,33 @@ Enter a number : 4
       *
     
 */
+
+/* 
+// practice session
+int main()
+{
+    int n;
+    cin >> n;
+
+    int i = 1;
+    while (i<=n)
+    {
+        int j = 1;
+        int space = 1;
+
+        while(space < i)
+        {
+            cout << "  ";
+            space += 1;
+        }
+
+        while(j<= n - i + 1)
+        {
+            cout << "* ";
+            j = j + 1;
+        }
+        cout << endl;
+        i += 1;
+    }
+}
+ */
