2c82c113f9c3028a3156b9c2239c7972c35a7992
Akash Jaiswal <akashjaiswal334@gmail.com>
2024-05-22 09:44:00 +0530

    Solving the shift operation right shift and left shift

---
+++ 3. Bitwise Operator/2_LeftRighShift.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout <<"Shift --> Right Sbift and Left Shift\n";
+
+    cout << (17>>2) << endl;
+    cout << (17>>1) << endl;
+    cout << (19<<1) << endl;
+    cout << (21<<2) << endl;
+    cout << (5>>1);
+    
+}
