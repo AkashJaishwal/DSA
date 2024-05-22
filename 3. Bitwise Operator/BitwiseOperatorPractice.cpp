bf65cc2887c9e3a93515046863cfe076a5783fcf
Akash Jaiswal <akashjaiswal334@gmail.com>
2024-05-22 09:23:10 +0530

    Bit wise Operators

--- 3. Bitwise Operator/BitwiseOperatorPractice.cpp
+++ 3. Bitwise Operator/BitwiseOperatorPractice.cpp
@@ -3,11 +3,16 @@ using namespace std;
 
 int main()
 {
-    int n , m; 
-    cin >> n;
-    cin >> m;
-
-
-    cout << "n & m" << n&m << endl;
+    int a, b;
     
+    cout <<"Bit Wise Operator\n"<<  "Enter two number : ";
+    cin >> a >> b;
+    
+    //Dealing with bitwise operators 
+    //they operates on bit level (Bit Operation)
+
+    cout <<endl << (a&b) << endl; // AND --> &
+    cout << (a|b) << endl; // OR --> |
+    cout << (~a) << endl; // NOT --> ~
+    cout << (a^b) << endl; // XOR --> ^
 }
