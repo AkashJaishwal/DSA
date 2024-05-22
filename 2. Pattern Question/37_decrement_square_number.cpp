19fe23cc5587202262bda17a7c63f33c4df7aca9
Akash Jaiswal <akashjaiswal334@gmail.com>
2024-05-22 20:20:23 +0530

    Solving the problem

--- 2. Pattern Question/37_decrement_square_number.cpp
+++ 2. Pattern Question/37_decrement_square_number.cpp
@@ -6,17 +6,49 @@ int main()
     int n;
     cin >> n;
 
-    int i = 1;
+    int a = 1; 
+    while ( a <= n)
+    {
+        // int b = 1;
+        // int count2 = n;
+        // while (b <= a){
+        //     cout << count2-- << " ";
+        //     b++;
+        // }
+
+        int c = 1;
+        int count3 = n;
+        while (c <= n){
+            cout << count3-- << " ";
+            c++;
+        }
+
+
+        cout << endl;
+        a++;
+    }
+
+    /* int i = 1;
     while (i <= n){
-        int j = i;
-        int k = 3;
-        while (j <= n){
-            while(k<n){
-            cout << (n-k++) << " ";
-            }
-            j = j + 1;
+        int j = 1;
+        int count = n;
+        while (j <= n - i + 1)
+        {
+            cout << count-- << " ";
+            j += 1;
+        }
+
+        int k = 1;
+        while (k <= (i-1)*2){
+            cout << i << " ";
+            k = k + 1;
+        }
+        int o = 1;
+        while (o <= n - i){
+            cout << o  + i << " ";
+            o++;
         }
         cout << endl;
         i++;
-    }
+    } */
 }
