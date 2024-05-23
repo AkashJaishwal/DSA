9963fc3faccf67a3bebf6744d1f69c09af44adc0
Akash Jaiswal <akashjaiswal334@gmail.com>
2024-05-23 09:09:16 +0530

    sum of n numbers

---
+++ 3. Bitwise Operator/4_For_loop.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter a Num for Sum : ";
+    cin >> n;
+    
+    int sum = 0;
+    for(int i=0; i<=n; i++)
+    {
+        cout << i << " ";
+        sum = sum + i;
+    }
+    cout << endl << sum;
+    return 0;
+}
