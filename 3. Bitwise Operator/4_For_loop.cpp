d353cbeb96551c23c308079151327280024c5fe0
Akash Jaiswal <akashjaiswal334@gmail.com>
2024-05-23 09:21:08 +0530

    Different cases for, for loop condition

--- 3. Bitwise Operator/4_For_loop.cpp
+++ 3. Bitwise Operator/4_For_loop.cpp
@@ -7,12 +7,23 @@ int main()
     cout << "Enter a Num for Sum : ";
     cin >> n;
     
-    int sum = 0;
-    for(int i=0; i<=n; i++)
-    {
-        cout << i << " ";
-        sum = sum + i;
+    // int sum = 0;
+    // for(int i=0; i<=n; i++)
+    // {
+    //     cout << i << " ";
+    //     sum = sum + i;
+    // }
+    // cout << endl << sum;
+    // return 0;
+
+    int i = 0;
+    for (;;){
+        if (i <= n){
+            cout << i << endl;
+            i++;
+        }
+        else {
+            break; 
+        }
     }
-    cout << endl << sum;
-    return 0;
 }
