ea11962a740e470ac59ea66d075ce77aef9673fe
Akash Jaiswal <akashjaiswal334@gmail.com>
2024-05-23 08:06:35 +0530

    Came up with better approach for Q37

--- 2. Pattern Question/37_decrement_square_number.cpp
+++ 2. Pattern Question/37_decrement_square_number.cpp
@@ -13,7 +13,7 @@ int main()
         int b = 1;
         int count2 = n;
         while (b <= a-1){
-            cout << count2 << " ";
+            cout << count2 << "  ";
             count2--;
             b++;
         }
@@ -21,14 +21,14 @@ int main()
         int c = 1;
         int count3 = n;
         while (c <= (n-a)*2){
-            cout << n - a + 1 << " ";
+            cout << n - a + 1 << "  ";
             c++;
         }
 
         int d = 1;
         int count4 = n;
         while (d <= a){
-            cout << count4 - a + 1<< " ";
+            cout << count4 - a + 1<< "  ";
             count4++;
             d++;
         }
@@ -42,21 +42,81 @@ int main()
         int count = n;
         while (j <= n - i + 1)
         {
-            cout << count-- << " ";
+            cout << count-- << "  ";
             j += 1;
         }
 
         int k = 1;
         while (k <= (i-1)*2){
-            cout << i << " ";
+            cout << i << "  ";
             k = k + 1;
         }
         int o = 1;
         while (o <= n - i){
-            cout << o  + i << " ";
+            cout << o  + i << "  ";
             o++;
         }
         cout << endl;
         i++;
     }
 }
+
+
+//Some other better approach 
+
+/* #include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a num: ";
+    cin >> n;
+    int size = 2 * n - 1;
+
+    int i = 0;
+    while (i < size) {
+        int j = 0;
+        while (j < size) {
+            int min_dist = min(min(i, j), min(size - i - 1, size - j - 1));
+            cout << n - min_dist << " ";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+}
+ */
+
+
+/* #include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a num: ";
+    cin >> n;
+    int size = 2 * n - 1;
+
+    int i = 0;
+    while (i < size) {
+        int j = 0;
+        while (j < size) {
+            int top = i;
+            int left = j;
+            int bottom = size - i - 1;
+            int right = size - j - 1;
+            
+            int min_dist = top; // initialize with top
+            if (left < min_dist) min_dist = left;
+            if (bottom < min_dist) min_dist = bottom;
+            if (right < min_dist) min_dist = right;
+
+            cout << n - min_dist << " ";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+
+    return 0;
+} */
--- 2. Pattern Question/tempCodeRunnerFile.cpp
+++
@@ -1,7 +0,0 @@
-int b = 1;
-        // int count2 = n;
-        // while (b <= (a-1)){
-        //     cout << count2 << " ";
-        //     count2 --;
-        //     b++;
-        // }
