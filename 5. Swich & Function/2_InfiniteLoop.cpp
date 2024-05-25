549ddd922d67bfd0f63edee3fc199e0367bfb42b
Akash Jaiswal <akashjaiswal334@gmail.com>
2024-05-25 20:26:15 +0530

    Infinite Loop continue not valid in switch cases

--- 5. Swich & Function/2_InfiniteLoop.cpp
+++ 5. Swich & Function/2_InfiniteLoop.cpp
@@ -15,6 +15,7 @@ int main()
             case 4 : cout << "Four"; break;
             case 5 : cout << "Five"; break;
             case 6 : cout << "Six"; break;
+            // continue; // It it not valid in switch case as it creat the continous loop and don't allow for the default cases
             case 7 : cout << "Seven"; break;
             case 8 : cout << "Eight"; break;
             case 9 : cout << "Nine"; break;
