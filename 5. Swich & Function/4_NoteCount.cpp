358cad3cea4e498bcba2e666a56d37df476e7f63
Akash Jaiswal <akashjaiswal334@gmail.com>
2024-05-26 23:30:48 +0530

    Lets Start with Function

--- 5. Swich & Function/4_NoteCount.cpp
+++ 5. Swich & Function/4_NoteCount.cpp
@@ -7,16 +7,18 @@ int main()
     cout << "Enter a Num : ";
     cin >> n;
 
-    int count = 0;
-    int num = 1;
-
-    switch(num){
-        case 1 : cout << n/100 << " 100 Notes" << endl;
-            n = n%100;
-        case 2 : cout << n/50 << " 50 Notes" << endl;
-            n = n%50;
-        case 3 : cout << n/20 << " 20 Notes" << endl;
-            n = n%20;
-        case 4 : cout << n/1 << " 1 Notes";
+    switch (1)
+    {
+    case 1:
+        cout << n / 100 << " 100 Notes" << endl;
+        n = n % 100;
+    case 2:
+        cout << n / 50 << " 50 Notes" << endl;
+        n = n % 50;
+    case 3:
+        cout << n / 20 << " 20 Notes" << endl;
+        n = n % 20;
+    case 4:
+        cout << n / 1 << " 1 Notes";
     }
 }
