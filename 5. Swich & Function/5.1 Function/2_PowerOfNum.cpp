51d005af4bd40de1a0c0cc420bea6d9f9b684caa
Akash Jaiswal <akashjaiswal334@gmail.com>
2024-05-27 10:04:28 +0530

    Little changes

--- 5. Swich & Function/5.1 Function/2_PowerOfNum.cpp
+++ 5. Swich & Function/5.1 Function/2_PowerOfNum.cpp
@@ -1,18 +1,37 @@
 #include <iostream>
 using namespace std;
 
-int power (int a, int b){
+/* int power (int a, int b){
     int ans = 1;
     for(int i = 1; i<= b; i++)
     {
         ans = ans * a;
     }
-    cout << ans;
+    return ans;
 }
 
 int main()
 {
     int a, b;
     cin >> a >> b;
-    power(a, b);
+    int result = power(a, b);
+    cout << result;
+} */
+
+int power (){
+    int a, b;
+    cin >> a >> b;
+    int ans = 1;
+    for(int i = 1; i<= b; i++)
+    {
+        ans = ans * a;
+    }
+    return ans;
+}
+
+int main()
+{
+    // int a, b;
+    int result = power();
+    cout << "Answer is : " << result;
 }
