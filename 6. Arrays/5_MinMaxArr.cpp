bd6b86187c975eb22ea75357313a4ea40a7a0532
Akash Jaiswal <akashjaiswal334@gmail.com>
2024-06-01 19:10:40 +0530

    Done with min and max

---
+++ 6. Arrays/5_MinMaxArr.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <limits.h>
+
+using namespace std;
+
+int getMax(int num[], int n)
+{
+    int max = INT_MIN; // This calls from from limits.h 
+    
+    for (int i=0; i<n; i++){
+        if(num[i] > max){
+            max = num[i];
+        }
+    }
+    //return the maximum value
+    return max;
+}
+
+int getMin(int num[], int n)
+{
+    int min = INT_MAX; // This calls from from limits.h 
+    for (int i=0; i<n; i++){
+        if(num[i] < min){
+            min = num[i];
+        }
+    }
+    //return the minimum value
+    return min;
+}
+
+int main()
+{
+    int size;
+    cout << "Enter size of array : ";
+    cin >>size;
+
+    int num[100];
+
+    for(int i=0; i<size; i++){
+        cin >> num[i];
+    }
+
+    // calling the function
+    cout << "Max in array is : " << getMax(num,size) << endl;
+    cout << "Min in array is : " << getMin(num,size);
+
+    return 0;
+
+}
