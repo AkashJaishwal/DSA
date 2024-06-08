ce1a0df2984ca03454b64b32e97531984df97b71
Akash Jaiswal <akashjaiswal334@gmail.com>
2024-06-08 23:36:50 +0530

    Practiced array problem

--- 6. Arrays/10_Practice.cpp
+++ 6. Arrays/10_Practice.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/* #include <iostream>
 using namespace std;
 
 void reverseArray(int arr[], int size)
@@ -42,4 +42,42 @@ int main()
     printArray(arr, n);
 
     return 0;
+}
+ */
+
+#include <iostream>
+using namespace std;
+
+void reverseArray(int arr[], int size){
+    int start = 0;
+    int end = size-1;
+
+    while (start <= end){
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
+void printArray(int arr[], int size){
+    for(int i=0; i<size; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+int main(){
+    int n;
+    cout << "Enter a size of array : ";
+    cin >> n;
+
+    cout << "Enter elements of array : ";
+    int arr[100];
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+
+    reverseArray(arr, n);
+    cout << "Reverse array is : \n";
+
+    printArray(arr, n);
 }
