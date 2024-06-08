11abbbfaaec5d2e0077e860993af88db396042f9
Akash Jaiswal <akashjaiswal334@gmail.com>
2024-06-08 23:21:37 +0530

    Practice array

---
+++ 6. Arrays/10_Practice.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+void reverseArray(int arr[], int size)
+{
+    int start = 0;
+    int end = size - 1;
+
+    while (start <= end)
+    {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter array Size : ";
+    cin >> n;
+
+    cout << "Enter element : " << endl;
+    int arr[100];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    reverseArray(arr, n);
+
+    cout << "Reverse array is : \n";
+    printArray(arr, n);
+
+    return 0;
+}
