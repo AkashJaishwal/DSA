4308887beeee2864e18988da426885cf593bab0c
Akash Jaiswal <akashjaiswal334@gmail.com>
2024-06-09 21:34:00 +0530

    Update

--- 6. Arrays/10_Practice.cpp
+++ 6. Arrays/10_Practice.cpp
@@ -1,4 +1,6 @@
-/* #include <iostream>
+/* 
+
+#include <iostream>
 using namespace std;
 
 void reverseArray(int arr[], int size)
@@ -43,36 +45,44 @@ int main()
 
     return 0;
 }
- */
+
+*/
 
 #include <iostream>
 using namespace std;
 
-void reverseArray(int arr[], int size){
+void reverseArray(int arr[], int size)
+{
     int start = 0;
-    int end = size-1;
+    int end = size - 1;
 
-    while (start <= end){
+    while (start <= end)
+    {
         swap(arr[start], arr[end]);
         start++;
         end--;
     }
 }
 
-void printArray(int arr[], int size){
-    for(int i=0; i<size; i++){
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
         cout << arr[i] << " ";
     }
     cout << endl;
 }
-int main(){
+
+int main()
+{
     int n;
     cout << "Enter a size of array : ";
     cin >> n;
 
     cout << "Enter elements of array : ";
     int arr[100];
-    for(int i=0; i<n; i++){
+    for (int i = 0; i < n; i++)
+    {
         cin >> arr[i];
     }
 
