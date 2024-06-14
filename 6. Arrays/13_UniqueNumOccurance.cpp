f45abd27136b7cfe8ce9d9d5b6b97b320ed78ce9
Akash Jaiswal <akashjaiswal334@gmail.com>
2024-06-14 20:11:56 +0530

    update

---
+++ 6. Arrays/13_UniqueNumOccurance.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+bool uniqueOccurrences(vector<int>& arr) {
+    
+}
+
+int main(){
+    int n;
+    cout << "Enter array size : ";
+    cin >> n;
+
+    cout << "Enter elements : " << endl;
+    int arr[100];
+    for(int i=0; i<n; i++){
+        cin >> arr[i];
+    }
+}
