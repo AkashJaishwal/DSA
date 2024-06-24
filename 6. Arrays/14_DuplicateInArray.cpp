e57632eb248abbc05c3473bdee4f49dafa139761
Akash Jaiswal <akashjaiswal334@gmail.com>
2024-06-24 21:40:16 +0530

    commit

--- 6. Arrays/14_DuplicateInArray.cpp
+++ 6. Arrays/14_DuplicateInArray.cpp
@@ -1,16 +1,40 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+void findDuplicate(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        bool isDuplicate = false;
+        for (int j = 0; j < i; j++) {
+            if (arr[i] == arr[j]) {
+                isDuplicate = true;
+                break;
+            }
+        }
+        if (isDuplicate) continue;
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                cout << arr[i] << " ";
+                break;
+            }
+        }
+    }
+}
+
+int main() {
     int n;
-    cout << "Enter array size : "; cin >> n;
+    cout << "Enter array size: ";
+    cin >> n;
 
     int arr[100];
-    cout << "Enter Element : ";
-    for(int i=0; i<n; i++){
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
         cin >> arr[i];
-        cout << arr[i] << " ";
     }
-    cout << "Array is : ";
-    
+
+    cout << "Duplicates : ";
+    findDuplicate(arr, n);
+    cout << endl;
+
+    return 0;
 }
