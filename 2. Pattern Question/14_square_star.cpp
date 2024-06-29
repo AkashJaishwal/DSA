1e64d8c7ad28da5ad0148ce1c3ef17300d1ccbf6
Akash Jaiswal <akashjaiswal334@gmail.com>
2024-06-29 23:13:44 +0530

    update

--- 2. Pattern Question/14_square_star.cpp
+++ 2. Pattern Question/14_square_star.cpp
@@ -1,19 +1,32 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
     cout << "Enter the number for Square print of '*' : ";
     cin >> n;
-    int i=1;
-    while(i <= n){ // we have to print * till n num so it it = in the i<=n suppose this will print the rows 
-        int j = 1; // here we go with the column 
-        while (j <= n){ // we are making j for column which will also be the same 
-            cout << "* ";
-            j = j + 1; // after every star print the while loop will check for the condition and here the value is added by 1 increment;
+    // int i = 1;
+    // while(i <= n){ // we have to print * till n num so it it = in the i<=n suppose this will print the rows
+    //     int j = 1; // here we go with the column
+    //     while (j <= n){ // we are making j for column which will also be the same
+    //         cout << "* ";
+    //         j = j + 1; // after every star print the while loop will check for the condition and here the value is added by 1 increment;
+    //     }
+    //     cout << endl;
+    //     i = i + 1; // Here we have not used the ++ operator as it will go into the infinite loop
+    // }
+    // return 0;
+    int i = 1;
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= n)
+        {
+            cout << " * ";
+            j++;
         }
         cout << endl;
-        i = i + 1; // Here we have not used the ++ operator as it will go into the infinite loop
+        i++;
     }
-    return 0;
 }
