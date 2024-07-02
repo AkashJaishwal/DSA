b125e82539ccc90e583494b46849add161a3e0d3
Akash Jaiswal <akashjaiswal334@gmail.com>
2024-07-02 23:41:52 +0530

    Update

---
+++ 6. Arrays/15_common.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int common(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for(int j = 1; j < size; j++){
+            if (arr[i]=arr[j]){
+                return arr[i];
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter a num : ";
+    cin >> n;
+
+    int arr[100];
+    cout << "Enter list : ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << "The common is : " << common(arr,n);
+}
