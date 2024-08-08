db9ec4deb079ef99bb1ca47d721f7dcb89c738aa
Akash Jaiswal <akashjaiswal334@gmail.com>
2024-08-08 17:02:28 +0530

    Updated with practice session

--- 6. Arrays/11_SwapAlternate.cpp
+++ 6. Arrays/11_SwapAlternate.cpp
@@ -1,7 +1,26 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void swapAlternate(int arr[], int size){
+void swapAlternate(int arr[], int size)
+{
+    int start = 0, next = 1;
+    while (next < size)
+    {
+        swap(arr[start], arr[next]);
+        start = start + 2;
+        next = next + 2;
+    }
+}
+
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+}
+
+/* void swapAlternate(int arr[], int size){
     int start = 0, next = 1;
     while(next<size){
         swap(arr[start], arr[next]);
@@ -14,16 +33,20 @@ void printArray(int arr[], int size){
     for(int i=0; i<size; i++){
         cout << arr[i] << " ";
     }
-}
+} */
 
-int main(){
+
+
+int main()
+{
     int n;
     cout << "Enter array size : ";
     cin >> n;
 
     cout << "Enter elements : " << endl;
     int arr[100];
-    for(int i=0; i<n; i++){
+    for (int i = 0; i < n; i++)
+    {
         cin >> arr[i];
     }
 
