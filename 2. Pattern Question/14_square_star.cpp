149bafdb68e5f2c131ff0c919d9a7d189e15febe
Akash Jaiswal <akashjaiswal334@gmail.com>
2024-08-13 07:31:43 +0530

    Practice session

--- 2. Pattern Question/14_square_star.cpp
+++ 2. Pattern Question/14_square_star.cpp
@@ -30,3 +30,20 @@ int main()
         i++;
     }
 }
+
+/* int main(){
+    int n=5;
+    
+    int i = 1;
+    while(i <= n)
+    {
+        int j = 1;
+        while (j <= n)
+        {
+            cout << j << " ";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+} */
