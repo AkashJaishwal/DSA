73c5d29723918cc2c919201628033da4e61adb8b
Akash Jaiswal <akashjaiswal334@gmail.com>
2025-03-26 02:53:40 +0530

    changes

--- 3. Bitwise Operator/5_Fibonacci_series.cpp
+++ 3. Bitwise Operator/5_Fibonacci_series.cpp
@@ -1,25 +1,44 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    int n;
-    cout << "Enter a Num for Sum : ";
-    cin >> n;
+// int main()
+// {
+//     int n;
+//     cout << "Enter a Num for Sum : ";
+//     cin >> n;
 
-    int a = 0;
-    int b = 1;
+//     int a = 0;
+//     int b = 1;
     
-    cout << a << " " << b << " ";
-    for(int i=1; i<=n-2; i++){ // n-2 cause we are printing a & b above outside the loop which is 2 digit
-        int nextNum = a + b;
-        cout << nextNum << " ";
+//     cout << a << " " << b << " ";
+//     for(int i=1; i<=n-2; i++){ // n-2 cause we are printing a & b above outside the loop which is 2 digit
+//         int nextNum = a + b;
+//         cout << nextNum << " ";
 
-        a = b;
-        b = nextNum;
-    }
-}
+//         a = b;
+//         b = nextNum;
+//     }
+// }
 /* 
 Enter a Num for Sum : 10
 0 1 1 2 3 5 8 13 21 34 
  */
+
+
+int main (){
+    int n;
+    cin >> n;
+
+    int x= 0;
+    int y = 1;
+
+    cout << x << " " << y  << " ";
+    for (int i = 1; i<=n-2; i++){
+        int next = x+y;
+        cout << next << " ";
+
+        x= y;
+        y = next;
+        
+    }
+}
--- 6. Arrays/test.cpp
+++ 6. Arrays/test.cpp
@@ -1,54 +1,21 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+int i = 4;
 
-using namespace std;
+switch(i) {
 
-long getMinimumOperations(const vector<int>& efficiency, const vector<int>& incr) {
-    int n = efficiency.size();
-    vector<pair<int, int>> layers(n);
-    
-    for (int i = 0; i < n; ++i) {
-        layers[i] = {efficiency[i], i};
-    }
-    
-    sort(layers.begin(), layers.end());
-    
-    vector<int> target_positions(n);
-    for (int i = 0; i < n; ++i) {
-        target_positions[layers[i].second] = i;
-    }
-    
-    long min_operations = 0;
-    vector<int> current_positions(n, 0);
-    
-    for (int i = 0; i < n; ++i) {
-        int idx = layers[i].second;
-        int target_position = target_positions[idx];
-        int current_position = current_positions[idx];
-        int increment_value = incr[idx];
-        
-        if (current_position < target_position) {
-            long required_increments = (target_position - current_position + increment_value - 1) / increment_value;
-            min_operations += required_increments;
-            current_positions[idx] += required_increments * increment_value;
+    default: ;
+
+    case 3:
+        i += 5;
+        if (i == 8) {
+            i++;
+            id (i == 9)
+            break;
+            i *= 2;
         }
-        
-        // Update subsequent positions if the current one changes
-        for (int j = idx + 1; j < n; ++j) {
-            current_positions[j] = max(current_positions[j], current_positions[idx]);
-        }
-    }
-    
-    return min_operations;
+        i -= 4;
+        break;
+    case 8: i+=5;
+        break;
 }
 
-int main() {
-    vector<int> efficiency = {4, 2, 7};
-    vector<int> incr = {3, 2, 1};
-    
-    long result = getMinimumOperations(efficiency, incr);
-    cout << result << endl;
-    
-    return 0;
-}
+print(i);
